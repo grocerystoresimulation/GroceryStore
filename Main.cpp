4f3bbe4362bcbfdc77437404aa58eefda2502704
doxuan994 <doxuancntp@gmail.com>
2018-03-25 12:07:01 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,19 @@
+#pragma once
+#include "Timer.h"
+#include "Customer.h"
+#include "Simulation.h"
 
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+	Simulation s;
+	s.startSimulation();
+
+
+	system("Pause");
+	return 0;
+}
