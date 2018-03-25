7f2e6bce43bf120d848bc71cdce7838d124b7a5c
doxuan994 <doxuancntp@gmail.com>
2018-03-25 16:41:38 -0400

    Update Timer.h

--- Timer.h
+++ Timer.h
@@ -1,69 +1,20 @@
-#pragma once
-
-
-
-#include <cassert>
-
 #include <iostream>
 using namespace std;
 
-
-
-/****************************************************************************************
-* Timer class keeps track of time in this Simulation.
-* The "minute" time unit is used in this Simulation.
-*
-*
-*
-****************************************************************************************/
-
-
+/******************************************************************
+ * Timer class keeps track of time in this Simulation.
+ * The "minute" time unit is used in this Simulation.
+ *****************************************************************/
 class Timer
 {
-
-
 public:
+	typedef int value_type;
+	typedef std::size_t size_type;
 
-	// Default constructor
-
-	Timer()
-	{
-		minutes = 0;
-	}
-
-	// Set minutes to the passed value
-
-	void setMinutes(int minutes)
-	{
-
-		assert(minutes >= 0);
-		this->minutes = minutes;
-
-	}
-
-
-	void tick()
-	{
-		minutes--;
-	}
-
-
-
-
-	// Return the number of minutes remaning in the simulation
-
-	int timeRemaining() const
-	{
-		return minutes;
-	}
-
-
+	Timer();
+	void setMinutes(value_type minutes); // Set minutes to the passed value
+	void tick();
+	value_type timeRemaining() const;
 private:
-
-	int minutes;
-
-
-
-
-
+	value_type minutes;
 };
