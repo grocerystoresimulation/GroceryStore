f52f0dbcf7d2be856c15d2890cde37fcc481c233
doxuan994 <doxuancntp@gmail.com>
2018-03-25 16:40:54 -0400

    Update Customer.h

--- Customer.h
+++ Customer.h
@@ -1,80 +1,23 @@
-#pragma once
-
-
 #include "Timer.h"
-
 #include <iostream>
 using namespace std;
 
-
 /****************************************************************************************
-* Customer class creates a Customer that has a specified time he or she can come to
-* check out and a specified time that a cashier can service this customer.
-*
-*
-*
-****************************************************************************************/
-
+ * Customer class creates a Customer that has a specified time he or she can come to
+ * check out and a specified time that a cashier can service this customer.
+ ***************************************************************************************/
 class Customer
 {
-
 public:
+	typedef int value_type;
+	typedef std::size_t size_type;
 
-
-	// Default constructor
-
-	Customer()
-	{
-
-		arrivalTime = 0;
-		serviceTime = 0;
-
-	}
-
-	// Parameterized constructor
-
-	Customer(const Timer & arrivalTime, int serviceTime)
-	{
-
-		this->arrivalTime = arrivalTime.timeRemaining();
-
-		// Set the service time
-		this->serviceTime = serviceTime;
-
-	}
-
-	// Sets the service time for a customer.
-
-	void setServiceTime(int serviceTime)
-	{
-		this->serviceTime = serviceTime;
-	}
-
-
-	// Returns the arrival time.
-
-	int getArrivalTime()
-	{
-		return arrivalTime;
-	}
-
-	// Returns a reference of the service time.
-
-	int & getServiceTime()
-	{
-		return serviceTime;
-	}
-
-
-
-
-
-
-
+	Customer();
+	Customer(const Timer & arrivalTime, value_type serviceTime);
+	void setServiceTime(value_type serviceTime);
+	value_type getArrivalTime();
+	value_type & getServiceTime();
 private:
-
-	int arrivalTime; // Time the customer arrived
-	int serviceTime; // Time the customer is serviced
-
-
+	value_type arrivalTime; // Time the customer arrived
+	value_type serviceTime; // Time the customer is serviced
 };
